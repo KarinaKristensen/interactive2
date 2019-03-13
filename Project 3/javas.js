156ede7db7913865791e574ea946d76b9e73d81f
Karina Kristensen <kkristensen@cca.edu>
2019-03-13 09:43:59 -0700

    doneeee

--- Project 3/javas.js
+++ Project 3/javas.js
@@ -4,14 +4,86 @@ $(document).ready(function(){
     
 });
 
-function makeNewPosition(){
+let mouseX = 0
+let mouseY = 0
+
+
+document.addEventListener('mousemove',function(event){
+    let element = event.path[0]
+    let parent = event.path[1]
+    mouseX = event.x
+    mouseY = event.y
+
+    // if(element.classList.contains('circle') && parent.classList.contains('right')){
+        
+    //     var w = $(window).width()/2 - 100;
+    //     var h = $(window).height()/2 - 100;
+
+    //     if(mouseY > window.innerHeight/2){
+    //         var nh = Math.floor(Math.random() * h);    
+    //     } else {
+    //         var nh = $(window).height()/2 + Math.floor(Math.random() * h);    
+    //     }
+
+        
+    //     var nw = Math.floor(Math.random() * w);
+    //     if(!$(element).is(':animated')){
+    //         $(element).animate({ top: nh, left: nw }, 1000);
+    //     }
+
+
+    // }
+
+                                        if(mouseX < window.innerWidth/2){
+
+                                        } else {
+                                            
+                                        }
+
+
+                                        
+                                    })
+
+function makeNewPosition(counter){
     
     // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 50;
-    var w = $(window).width()/2 - 50;
+    var h = $(window).height() - 100;
+    var w = $(window).width()/2 - 100;
     
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+
+    var nh = 0
+    var nw = 0
+
+
+    if(mouseX < window.innerWidth/2 && counter < 4){
+        // left side 
+        console.log('left side left circle')
+        nh = mouseY - 50 + Math.random() * 100 - 50
+        nw = mouseX - 50 + Math.random() * 100 - 50
+
+
+    } else if(mouseX >= window.innerWidth/2 && counter >= 4){
+        // right side
+        console.log('right right circle')
+
+        h = $(window).height()/2 - 100;
+
+        
+        nw = Math.floor(Math.random() * w);
+        if(mouseY > window.innerHeight/2){
+            nh = Math.floor(Math.random() * h);    
+        } else {
+            nh = $(window).height()/2 + Math.floor(Math.random() * h);    
+        }
+
+
+    } else {
+        console.log('everyone else')
+        nh = Math.floor(Math.random() * h);
+        nw = Math.floor(Math.random() * w);
+    }
+
+    
     
     return [nh,nw];    
     
@@ -22,16 +94,27 @@ function animateDiv(myclass){
     let elements = document.querySelectorAll(myclass)
     for(let counter = 0; counter < elements.length; counter ++){
 
-        var newq = makeNewPosition();
+        var newq = makeNewPosition(counter);
 
-        $(elements[counter]).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
-          if(counter == 0) {
-                animateDiv(myclass);        
-            }
-        });
+        if(newq[0] != 0 && newq[1] != 0){
+
+            $(elements[counter]).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+              if(counter == 0) {
+                    animateDiv(myclass);        
+                }
+            });
+        }
     }
     
 };
+
+
+
+
+
+
+
+
 
 
 // let elements = []
